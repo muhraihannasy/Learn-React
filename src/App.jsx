934f18cf179c0ae546be1d233d0a74b88b570f56
muhraihannasy <muhraihannasy@gmail.com>
2023-05-10 14:47:19 +0700

    updating our directory page with directory component and category component item

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CategoryItem from "./components/category-item/category-item.component";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      image: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      image: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      image: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      image: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      id: 5,
+      title: "Mans",
+      image: "https://picsum.photos/seed/picsum/200/300",
+    },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="categories-container">
+        {categories.map((item) => (
+          <CategoryItem category={item} key={item.id} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import "./category-item.scss";
+
+const CategoryItem = ({ category }) => {
+  const { id, title, image } = category;
+
+  return (
+    <div className="category-container" key={id}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${image})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
